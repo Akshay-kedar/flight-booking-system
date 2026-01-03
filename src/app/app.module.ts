6ccc75d333295749a38c683b8f6556093047f0e2
Akshay Kedar <kedarakshay38@gmail.com>
2026-01-03 11:43:56 +0530

    adding header logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MessageService } from 'primeng/api';
 import { HeaderComponent } from './core/components/header/header.component';
+import { MenubarModule } from 'primeng/menubar';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    MenubarModule,
+    AvatarModule,
+    ToastModule,
+  ],
   providers: [MessageService],
   bootstrap: [AppComponent],
 })
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isLoggedIn = false;
+  items: MenuItem[] = [];
+  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit(): void {
+    this.authService.isLoggedIn$.subscribe((loggedIn) => {
+      this.isLoggedIn = loggedIn;
+    });
+  }
 
+  updateMenu() {
+    this.items = [
+      {
+        label: 'FlyHigh',
+        icon: 'pi pi-fw pi-send',
+        routerLink: '/',
+      },
+      {
+        label: 'search Flights',
+        icon: 'pi pi-fw pi-search',
+        routerLink: '/booking/search',
+        visible: this.isLoggedIn,
+      },
+      {
+        label: 'New Booking',
+        icon: 'pi pi-fw pi-plus',
+        routerLink: '/booking',
+        visible: this.isLoggedIn,
+      },
+    ];
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
 }
